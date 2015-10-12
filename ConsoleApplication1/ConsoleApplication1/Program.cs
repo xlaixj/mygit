c53df05743a1fabdb0666c33c035a0704999f7d6
qiuxianlin <xianlin@live.com>
2015-10-12 13:35:24 +0800

    csharp

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.Write("Hello World");
+            System.Console.ReadKey();
+        }
+    }
+}
